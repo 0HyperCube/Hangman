796e5702a7cdae79c0bb435c924cd35efc25d08a
HyperCube <0hypercube@gmail.com>
2021-02-04 11:49:15 +0000

    Add basic static site

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    tide::log::start();
+    let mut app = tide::new();
+	app.at("/").serve_dir("Assets/")?;
+	app.at("/").serve_file("Assets/index.html")?;
+    app.listen("127.0.0.1:6543").await?;
+    Ok(())
+}

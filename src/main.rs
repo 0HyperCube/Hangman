f3cdc7bbf6496bbf7b6a442a0e56148701361d2a
HyperCube <0hypercube@gmail.com>
2021-02-04 20:38:42 +0000

    Minor style updates

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> Result<(), std::io::Error> {
 	tide::log::start();
 	let mut app = tide::new();
 	app.at("/").serve_dir("Assets/")?;
-	app.at("/").serve_file("Assets/index.html")?;
+	app.at("/").serve_file("Assets/Index.html")?;
 	app.listen("127.0.0.1:6543").await?;
 	Ok(())
 }

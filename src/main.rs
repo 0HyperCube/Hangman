8cdd7cb27d8bdea5c275f92054cf7ec817d8aa22
HyperCube <0hypercube@gmail.com>
2021-02-11 11:20:35 +0000

    Add some structs for the lobby

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,61 @@
-use serde::{Deserialize, Serialize};
-use tide::{http::mime, Request, Response};
 
+use serde::{Deserialize, Serialize};
+use tide::{Request, Response, http::{mime}};
+use std::{collections::LinkedList};
+
+
+
+#[derive(Serialize, Deserialize)]
+struct LobbyUser{
+	username : String,
+	last_fetch_action : u64,
+	is_joining : bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Lobby{
+	latest_action: u64,
+	users_in_lobby: LinkedList<LobbyUser>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ClientConnection {
+	username: String,
+}
 
 async fn connect(mut request: Request<()> ) -> tide::Result<impl Into<Response>> {
-	println!("got a connect request with a username object");
-	let text : &str = &request.take_body().into_string().await?;
-	println!("Got connect {}", text);
-	let query : Username = serde_json::from_str(text)?;
-	println!("Got name {}", query.name);
+	println!("Recieved new connection request");
+	let str : &str = &request.take_body().into_string().await?;
+	let client_connection : ClientConnection = serde_json::from_str(str)?;
+	println!("Got name {}", client_connection.username);
 	
+	// TODO: Add user to lobby
+
 
 	Ok(Response::builder(200).build())
 
 }
 
-async fn get_all_members(_: Request<()> ) -> tide::Result<impl Into<Response>> {
+async fn get_lobby(_: Request<()> ) -> tide::Result<impl Into<Response>> {
 	
-	let response : TestResponse = TestResponse{is_bob:true,height:43};
 
 	Ok(Response::builder(200)
-		.body(serde_json::to_string(&response)?)
+		.body("hi")
 		.content_type(mime::HTML))
-
-}
-
-#[derive(Serialize, Deserialize)]
-struct Username {
-	 name: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct TestResponse {
-	is_bob : bool,
-	height : u32,
 
 }
 
 #[async_std::main]
 async fn main() -> std::result::Result<(), std::io::Error> {
+	println!("{}", u64::MAX);
+
 	tide::log::start();
 	let mut app = tide::new();
+
 	app.at("/").serve_dir("Assets/")?;
 	app.at("/").serve_file("Assets/Index.html")?;
 	app.at("/connect").post(connect);
-	app.at("/getallmembers").get(get_all_members);
+	app.at("/getallmembers").get(get_lobby);
 	app.listen("127.0.0.1:6543").await?;
 	Ok(())
 	
